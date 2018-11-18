62036502a1d9235402d4a212a5b4477001d9b618
Pakillo <f.rodriguez.sanc@gmail.com>
2018-11-19 00:34:10 +0100

    clean empezando script

--- scripts/empezando.R
+++ scripts/empezando.R
@@ -3,142 +3,44 @@
 ## Rstudio projects
 
 ## R as calculator
-3 + 2
-10 - 8
-10000 * 2
-log(34)
-sqrt(12456667898)
 
-log(34) - sqrt(12456667898)
 
 
 ## Assignment
-objeto <- log(34)
-miraiz <- sqrt(4567890)
-miraiz
-diff <- (objeto - miraiz)/2
-rm(diff)
 
 
-## Vectors
-mivector <- c(1,2,3, 4, 5, 8)
-nombres <- c("Patricia", "Manuel", "Agustin")
-masnumeros <- c(5,3,7,13,21,2)
-dif <- mivector + masnumeros
-dif
-sqrt(mivector)
+
+## Vectors (numeric, characters)
+
 
 
 ## Indexing, logical subsetting, which
-mivector
-mivector[6]
-mivector[c(3,4,6)]
-mivector > 4
-mivector[mivector > 4]
-mivector*2
+
 
 ## NAs
-nuevosvals <- c(45, 32, NA, 12, NA, NA)
-nuevosvals*2
+
 
 
 ## Calling functions (sum, mean, log)
-mivector
-misuma <- sum(mivector)
-mean(mivector)
-log(mivector)
-sum(nuevosvals, na.rm = FALSE)
-sum(1, 2, 3, 4, 5)
-sum(1:5)
-1:5
-1:50
+
 
 ## Getting help: ?, ??, google, stackoverflow, Rdocumentation, Rseek, cheatsheets
-aleatorios <- runif(5, min = 0, max = 100)
-round(aleatorios)
-round(runif(5, min = 0, max = 100))
-
-n <- 23
-runif(n, min = 0, max = 50)
-
-
-
-
 
 
 ## Reading in data
 tinyurl.com/treesdata
 
-trees <- read.csv("datasets/trees.csv")
 
-## Data frames
-summary(trees)
-str(trees)
+## Data frames (summary, str, glance)
+
 
 ## Packages
 
-## dplyr
-library(dplyr)
-bigfemales <- filter(trees, height > 30,
-                      sex == "female", dbh > 25)
-bigfemales <- mutate(bigfemales, suma = height + dbh)
+## readr
 
-solovivos <- select(trees, -dead)
-solosex <- select(trees, sex)
-
-trees %>%
-  group_by(plot) %>%
-  summarise(max(dbh) + max(height))
-
-
-##
-
-miedad <- c(35, 23, 30, 28, 34, 31, 32, 33, 33, 30, 31)
-mean(miedad)
-hist(miedad)
+## dplyr (filter, select, mutate, arrange, distinct, slice, group_by, summarise...)
 
 
 
 ## ggplot2
-
-trees <- read.csv("datasets/trees.csv")
-library(ggplot2)
-
-ggplot(trees, aes(x = dbh, y = height)) +
-  geom_point() + theme(plot.title = element_text(size = 30,
-    hjust = 0.5), panel.background = element_rect(fill = "darkorange")) + labs(title = "mi titulo", x = "diametro")
-
-myplot <- ggplot(trees, aes(x = dbh, y = height))
-myplot <- myplot + geom_point()
-myplot
-
-
-# boxplot
-ggplot(trees, aes(x=factor(plot),y = height))+
-  geom_boxplot(aes(colour = sex))+
-  labs(x="Study plot",y="Height (m)", title="Tree height per plot")
-
-
-# violinplot
-ggplot(trees, aes(x=factor(plot),y = height))+
-  geom_violin(fill="orange")+
-  labs(x="Study plot",y="Height (m)", title="Tree height per plot") +
-  geom_point()
-
-
-
-## multipanel
-
-ggplot(trees, aes(x = dbh, y = height)) +
-  geom_point() +
-  facet_wrap(~sex)
-
-library(ggplot2)
-trees<-read.csv("datasets/trees.csv")
-ggplot(trees, aes(x=height))+
-  geom_histogram(aes(colour=sex))+
-  facet_wrap(~plot) +
-  labs(x = "Heights", y = "Number of trees", title = "Ole")
-
-
 
