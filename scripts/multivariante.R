1b51e2bcb5cc2c4ffd8d30bfd90e3448eff8e880
Nacho <ibartomeus_pro2@MacBook-Pro-2.local>
2020-01-23 10:31:06 +0100

    Update multivariante 2020

--- scripts/multivariante.R
+++ scripts/multivariante.R
@@ -2,26 +2,107 @@
 
 library(vegan)
 
-#PCA----
-data(iris)
+#Correlation
+data("iris")
+View(iris)
+summary(iris)
+str(iris)
 head(iris)
+iris
+
+plot(iris)
+
+iris$Sepal.Length
+iris[,"Sepal.Length"]
+iris[,1]
+
+plot(x = iris$Sepal.Length,
+     y = iris$Petal.Length,
+     col = "red")
+
+iris$color <- iris$Species
+levels(iris$color) <-
+  c("red", "purple", "orange")
+
+plot(x = iris$Sepal.Length,
+     y = iris$Petal.Length,
+     col = as.character(iris$color))
+
+
+plot(iris$Sepal.Length ~ iris$Petal.Length)
+scatter.smooth(iris$Sepal.Length ~ iris$Petal.Length)
+cor(iris$Sepal.Length, iris$Petal.Length)
+cor(iris$Sepal.Length, iris$Petal.Length, method = "spearman")
+cor.test(x = iris$Sepal.Length,
+         y = iris$Petal.Length)
+
+setosa_only <- subset(iris, Species == "setosa")
+#> < mayor/menos que
+# == igual que
+# != diferente de
+# & y
+# | o
+
+pairs(iris)
+cor(x = iris) #fails
+str(iris)
+
+cor(x = iris[,1:4])
+cor(x = iris[,c(1,2,3,4)])
+cor(iris[,-5])
+
+#PCA----
 
 #prepare and explore the data
 ir <- iris[, 1:4]
 ir_species <- iris[, 5]
 pairs(ir)
 cor(ir)
+
 #run pca
 #princomp
-pca <- prcomp(ir, center = TRUE,
-              scale. = TRUE)
-pca
-summary(pca)
-plot(pca, type = "l")
+
+pca <- prcomp(x = ir)
+summary(pca) #PC contributions
+plot(pca)
 biplot(pca)
-#or nicer:
+biplot(pca, choices = c(1,3))
+pca #loadings o pesos
+pca$x #scores o valores de cada observacion
+
+pca2 <- prcomp(x = ir, center = TRUE, scale. = TRUE)
+summary(pca2) #PC contributions
+plot(pca2)
+biplot(pca2, choices = c(1,2))
+pca2 #loadings o pesos
+pca2$x #scores o valores
+summary(iris)
+
+#predicción
+losdatosdelcolega <- data.frame(
+  Sepal.Length = c(4.3, 5.4),
+  Sepal.Width = c(2.5, 3.0),
+  Petal.Length = c(1.8, 2.0),
+  Petal.Width = c(0.5, 1.3))
+
+
+prediccion <- predict(pca2,
+                      newdata = losdatosdelcolega)
+
+prediccion
+
+names(pca2)
+#pca2$center
+#pca2$scale
+
+biplot(pca2)
+points(x = prediccion[,1],
+       y = prediccion[,2],
+       col = "red", pch = 19)
+
+#a nicer plot:
 library(devtools)
-install_github("ggbiplot", "vqv")
+install_github("vqv/ggbiplot")
 library(ggbiplot)
 g <- ggbiplot(pca, obs.scale = 1, var.scale = 1,
               groups = ir_species, ellipse = TRUE,
@@ -30,13 +111,15 @@ g <- g + scale_color_discrete(name = '')
 g <- g + theme(legend.direction = 'horizontal',
                legend.position = 'top')
 print(g)
-#more tricks
-predict(pca, newdata=(tail(log_ir, 2)))
+
 
 #NMDS and PERMANOVA----
 #The data
 Herbivores <- read.csv(file = "datasets/Herbivore_specialisation.csv", header = TRUE)
 head(Herbivores)
+str(herbivory)
+summary(herbivory)
+hist(herbivory$Mass)
 
 #simplify objects to use
 Habitat <- Herbivores$Habitat
@@ -44,11 +127,20 @@ DayNight <- Herbivores$DayNight
 #select the community
 Herb_community <- Herbivores[,5:11]
 
+#cargar librerias----
+#install.packages("vegan") #si no tienes la libreria, instalala primero
+library("vegan")
+
 #The basic is the distance measure you use:
 #distance selection!----
 ?dist
 ?vegdist
 ?betadiver
+
+v <- vegdist(Herb_community, "horn")
+head(v, 20)
+Herb_community[c(1,13),]
+vegdist(Herb_community[c(1,13),], "horn")
 
 #A few words:
 #binary:
@@ -72,9 +164,9 @@ Herb_community <- Herbivores[,5:11]
 Herb_community.mds <- metaMDS(comm = Herb_community,
                               distance = "bray",
                               trace = FALSE, autotransform = FALSE)
+#Herb_community.mds <- metaMDS(v) #idem
 plot(Herb_community.mds$points, col = Habitat, pch = 16)
 
-plot(Herb_community.mds$points, col = Habitat, pch = 16)
 Habitat.uni <- unique(Habitat)
 legend(x = -2.5, y = -1.5, Habitat.uni, pch=16, col = 1:5, cex = 0.8)
 
@@ -94,14 +186,21 @@ Herb_community.mds$stress
 
 Herb_community.sq <- sqrt(Herb_community)
 Herb_community.sq.mds <- metaMDS(comm = Herb_community.sq,
-                                 distance = "bray", trace = FALSE)
+                                 distance = "bray", autotransform = FALSE)
 plot(Herb_community.sq.mds$points, col = Habitat, pch = 16)
 
 #alternative plot
 ordiplot(Herb_community.mds,type="n")
+points(Herb_community.mds$points, cex = 0.5,
+       pch = 16,
+       col = "darkgrey")
 ordihull(Herb_community.mds,groups=Habitat,draw="polygon",col="grey90",
          label=FALSE)
 orditorp(Herb_community.mds,display="species",col="red",air=0.01)
+ordiellipse(ord = Herb_community.mds,
+            groups = Habitat,
+            col = "red")
+
 
 #Testing hypothesis PERMANOVA----
 #First test: Are centroids different?
